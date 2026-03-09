f8fdaa1be2644729e3b6a6926a14d2c4ff795c51
ALEXANDRE SILVA <alex.silva250@hotmail.com>
2026-03-09 13:07:21 -0300

    feat: project base created

--- src/main/java/br/com/libertadfacilities/blog/services/UserService.java
+++ src/main/java/br/com/libertadfacilities/blog/services/UserService.java
@@ -1,9 +1,12 @@
 package br.com.libertadfacilities.blog.services;
 
+import br.com.libertadfacilities.blog.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
 }
