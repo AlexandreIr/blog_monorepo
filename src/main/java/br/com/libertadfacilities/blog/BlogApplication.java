1e83a8203702b0f5e24926fdb565755d56017b8a
Alexandre <alex.silva250@hotmail.com>
2026-03-16 22:03:56 -0300

    fix: enviroment variable problem fixed

--- src/main/java/br/com/libertadfacilities/blog/BlogApplication.java
+++ src/main/java/br/com/libertadfacilities/blog/BlogApplication.java
@@ -1,5 +1,6 @@
 package br.com.libertadfacilities.blog;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv.configure().ignoreIfMissing().load();
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
