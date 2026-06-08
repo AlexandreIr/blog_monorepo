af6e420b6352080acac97e60d41233992a184af1
ALEXANDRE SILVA <alex.silva250@hotmail.com>
2026-06-08 15:18:56 -0300

    fix: fix on api route

--- blog-frontend/src/api/api.ts
+++ blog-frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://libertad-api.onrender.com/",
+  baseURL: "https://libertad-api.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
