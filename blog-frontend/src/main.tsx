b28c2028704589cd50cacc8a9b30376907d4b0b4
ALEXANDRE SILVA <alex.silva250@hotmail.com>
2026-06-30 17:30:21 -0300

    changes on master branch

--- blog-frontend/src/main.tsx
+++ blog-frontend/src/main.tsx
@@ -1,16 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 // @ts-ignore
 import "./style.css";
-import {ThemeProvider} from "./context/ThemeContext";
 
 // @ts-ignore
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <ThemeProvider>
-        <App />
-    </ThemeProvider>
+    <App />
   </BrowserRouter>
 );
