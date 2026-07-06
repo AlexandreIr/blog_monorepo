570b1b3bd1ca473654ef7dc8206a155f9cf6788e
ALEXANDRE SILVA <alex.silva250@hotmail.com>
2026-07-06 15:19:30 -0300

    feat: updates on SecurityConfig

--- blog-api/src/main/java/br/com/libertadfacilities/blog/security/SecurityConfig.java
+++ blog-api/src/main/java/br/com/libertadfacilities/blog/security/SecurityConfig.java
@@ -35,6 +35,9 @@ public class SecurityConfig {
     @Value("${app.frontend-url}")
     private String frontendUrl;
 
+    @Value("${app.blog-admin-url")
+    private String blogAdminUrl;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -62,7 +65,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", frontendUrl));
+        configuration.setAllowedOrigins(List.of(blogAdminUrl, frontendUrl));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
